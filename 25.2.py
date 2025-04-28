f1bbb509d1f084a0ceccbba1a09759804bb1bd24
OlehDe <oleh.r.diachok@ukd.edu.ua>
2025-04-28 09:29:29 +0300

    ,

--- 25.2.py
+++ 25.2.py
@@ -36,10 +36,10 @@ class Race:
         print(f"The winner is {winner}!")
 
 if __name__ == "__main__":
-    car1 = Car("BMW", "X5", 2022, 250, 300, "Black")
-    car2 = Car("Audi", "A6", 2023, 240, 280, "White")
-    driver1 = Driver("John", 35, 15)
-    driver2 = Driver("Alice", 28, 10)
+    car1 = Car("Lamborghini", "Jalpa", 2022, 250, 300, "Black")
+    car2 = Car("Bugatti", "Chiron", 2023, 240, 280, "White")
+    driver1 = Driver("Vasya", 35, 15)
+    driver2 = Driver("Dima", 28, 10)
     race = Race()
 
     winner = race.start_race(car1, car2)
